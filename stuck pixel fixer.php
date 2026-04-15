6fb5132470c481233261b8d0d1fac5f3abab5b9f
wedinio <wedinio777@gmail.com>
2026-04-15 16:03:23 -0500

    Implemented description for stuck pixel fixer

--- stuck pixel fixer.php
+++ stuck pixel fixer.php
@@ -226,7 +226,7 @@ get_header(); ?>
 	<br/><br/>
 	<div class="wid-sm-100 wid-xs-100">
 		<div class="ct-row mar-bot-15 dis-flex">
-			<!-- <img class="tve_image" alt="" style="width: 64px;" src="<?php the_field('icon'); ?>" width="64" height="64"> -->
+			<img class="tve_image" alt="" style="width: 64px;" src="<?php the_field('icon'); ?>" width="64" height="64">
 			<div class="webcam-1-text_">
 				<div class="icon-text-1">
 					<h3 class="ct-bold-text"><?php the_field('get_easily_started_title'); ?></h3>
@@ -251,11 +251,11 @@ get_header(); ?>
 				</ul>
 			</div>
 		</div>
-	</div>
+	</div>	
 
 	<div class="wid-sm-100 wid-xs-100">
 		<div class="ct-row mar-bot-15 dis-flex">
-			<!-- <img class="tve_image" alt="" style="width: 64px;" src="<?php the_field('red_icon'); ?>" width="64" height="64"> -->
+			<img class="tve_image" alt="" style="width: 64px;" src="<?php the_field('red_icon'); ?>" width="64" height="64">
 			<div class="webcam-1-text_">
 				<div class="icon-text-1">
 					<h3 class="ct-bold-text" style="color: rgb(226, 92, 27)"><?php the_field('trouble-shooting_title'); ?></h3>
@@ -264,47 +264,29 @@ get_header(); ?>
 		</div>
 
 		<div class="trouble-shooting-2 dis-flex">
-			<div class="width-33_3 wid-md-50 wid-xs-100">
+			<div class="wid-100 wid-md-100 wid-xs-100">
 				<div class="trouble-shooting-text-1 pd-1">
 					<ul>
 						<?php
-						if (have_rows('leftside_guide_list')):
-							while (have_rows('leftside_guide_list')): the_row(); ?>
-								<li>
-									<span class="fw-bold color-link">
-										<?php the_sub_field('left_side_list_title'); ?>
-									</span>
-								</li>
-							<?php endwhile;
-						endif;
-						?>
+
+                            // check if the repeater field has rows of data
+                            if (have_rows('leftside_guide_list')):
+
+                                // loop through the rows of data
+                            while (have_rows('leftside_guide_list')): the_row(); ?>
+												<li>
+													<span class="fw-bold color-link">
+														<?php the_sub_field('left_side_list_title'); ?>
+													</span>
+												</li>
+
+											<?php endwhile;
+                                                else:
+                                                endif;
+                                            ?>
 					</ul>
 				</div>
 
-				<div align="center">
-					<style>
-						.OMT_MOINSBD_Middle { width: 300px; height: 250px; }
-						@media(min-width: 500px) { .OMT_MOINSBD_Middle { width: 300px; height: 250px; } }
-						@media(min-width: 800px) { .OMT_MOINSBD_Middle { width: 300px; height: 250px; } }
-					</style>
-				</div>
-			</div>
-			<?php
-			$right_side_guide_list = get_field('rightside_guide_list');
-			if ($right_side_guide_list) { ?>
-				<div class="width-33_3 wid-md-50  wid-xs-100">
-					<div class="trouble-shooting-text-1 pd-1">
-						<ul>
-							<li>
-								<span class="fw-bold">
-									<?php echo $right_side_guide_list; ?>
-								</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-			<?php } ?>
-			<div class="width-33_3 md-hidden">
 			</div>
 		</div>
 	</div>
@@ -314,29 +296,31 @@ get_header(); ?>
 			<div class="ct-row dis-flex">
 				<div class="width-50 wid-xs-100">
 					<div class="read-more-text-secction">
-						<div class="read-more-title clearfix">
+						<div class="read-more-title clearfix" >
 							<h2><strong><?php the_field('more_about_title'); ?></strong></h2>
 						</div>
 						<?php
-						if (have_rows('test_content')):
+
+							// check if the repeater field has rows of data
+							if (have_rows('test_content')):
+
+								// loop through the rows of data
 							while (have_rows('test_content')): the_row(); ?>
 								<div class="read-more-1">
+
+
 									<div class="read-more-subtitle clearfix">
 										<h3 class="mar-bot-20"><?php the_sub_field('heading'); ?></h3>
 									</div>
+
 									<div class="read-more-text">
 										<p><?php the_sub_field('descp'); ?></p>
 									</div>
 								</div>
 							<?php endwhile;
-						endif;
+								else:
+								endif;
 						?>
-					</div>
-				</div>
-
-				<div class="width-50">
-					<div class="img-section pad-left-15">
-						<img class="lazyload" src="<?php the_field('rightside_lazy_gif'); ?>" data-src="<?php the_field('rightside_image'); ?>"/>
 					</div>
 				</div>
 			</div>
