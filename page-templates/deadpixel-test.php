a20e36bcc380f30b238dfbd0782074d3da5fc6df
gradaom <golden.peach.ts@gmail.com>
2026-05-20 16:55:50 -0400

    Fixed the issue of dead pixel button

--- page-templates/deadpixel-test.php
+++ page-templates/deadpixel-test.php
@@ -1,6 +1,15 @@
 <?php
     /* Template Name:Dead Pixel Test */
-get_header(); ?>
+get_header();
+
+$test_btn_width = '180px';
+if (function_exists('pll_current_language')) {
+	$lang = pll_current_language();
+	if ($lang === 'fr' || $lang === 'vi') {
+		$test_btn_width = '300px';
+	}
+}
+?>
 <style media="screen">
 	@media all and (max-width: 1024px) {
 		#sAs-menu-responsive span {
@@ -21,9 +30,10 @@ get_header(); ?>
 		background-color: #e25d26;
 		border: 1px solid #e25d26;
 		border-radius: 5px;
-		padding: 6px;
-		width: 180px;
+		padding: 6px 16px;
+		width: <?php echo esc_attr($test_btn_width); ?>;
 		height: 50px;
+		white-space: nowrap;
 		font-size: 24px;
   	}
 
